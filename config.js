731a9fc72c834ea54e8aee346eec2ad9a74c3d94
Ivan Iufriakov <tomatyss@gmail.com>
2024-04-23 18:22:23 -0400

    Update config.js to use the latest version of gemini-1.5-pro

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export const config = {
   connectorName: "Gemini",
-  models: ["gemini-1.0-pro", "gemini-1.5-pro"],
+  models: ["gemini-1.0-pro", "gemini-1.5-pro-latest"],
   properties: [
     {
       id: "maxOutputTokens",
